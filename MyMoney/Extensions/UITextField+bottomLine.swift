fd3b337e3bda113d18f29d77542947124c0cb31f
Jarakys <chernovkirilldev@gmail.com>
2020-08-05 16:37:54 +0300

    first commit

---
+++ MyMoney/Extensions/UITextField+bottomLine.swift
@@ -0,0 +1,9 @@
+//
+//  UITextField+bottomLine.swift
+//  MyMoney
+//
+//  Created by Kirill on 16.02.2020.
+//  Copyright © 2020 Samax. All rights reserved.
+//
+
+import Foundation
---
+++ MyMoney/ViewControllers/Auth/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  MyMoney
+//
+//  Created by Kirill on 15.02.2020.
+//  Copyright © 2020 Samax. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
